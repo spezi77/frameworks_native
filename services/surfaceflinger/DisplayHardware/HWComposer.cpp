e09957d1b48afba0d38e32881cc65dbb62aa68a7
spezi77 <spezialitz@gmx.de>
2014-03-27 18:53:17 +0100

    ifdef HWC_BLIT
    Change-Id: If67c8ae70fadb1309990336ae97fb7da2dd62c7d
    
    Conflicts:
    
    	services/surfaceflinger/DisplayHardware/HWComposer.cpp
    	services/surfaceflinger/Layer.cpp
    	services/surfaceflinger/SurfaceFlinger.cpp

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -776,9 +776,11 @@ status_t HWComposer::prepare() {
                         }
                         // If the composition type is BLIT, we set this to
                         // trigger a FLIP
+#ifdef QCOM_BSP
                         if(l.compositionType == HWC_BLIT) {
                             disp.hasFbComp = true;
                         }
+#endif
                         if (l.compositionType == HWC_OVERLAY) {
                             disp.hasOvComp = true;
                         }
@@ -1371,6 +1373,9 @@ void HWComposer::dump(String8& result) const {
                             "HWC",
                             "BACKGROUND",
                             "FB TARGET",
+#ifdef QCOM_BSP
+                            "FB_BLIT",
+#endif
                             "UNKNOWN"};
                     if (type >= NELEM(compositionTypeName))
                         type = NELEM(compositionTypeName) - 1;
--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -444,7 +444,11 @@ void Layer::setAcquireFence(const sp<const DisplayDevice>& hw,
     // TODO: there is a possible optimization here: we only need to set the
     // acquire fence the first time a new buffer is acquired on EACH display.
 
-    if (layer.getCompositionType() == HWC_OVERLAY) {
+    if (layer.getCompositionType() == HWC_OVERLAY
+#ifdef QCOM_BSP
+		    || layer.getCompositionType() == HWC_BLIT
+#endif
+	    ) {
         sp<Fence> fence = mSurfaceFlingerConsumer->getCurrentFence();
         if (fence->isValid()) {
             fenceFd = fence->dup();
