7a8d0b3130be2119573d7920b464f73ee178bdbe
spezi77 <spezialitz@gmx.de>
2014-03-27 19:04:54 +0100

    Revert "Add support for custom buffer sizes.."
    This reverts commit d6eecf50919312c1dd778c5742de72bb98f98f1a.
    
    Conflicts:
    
    	libs/gui/Android.mk
    	libs/ui/Android.mk
    	libs/ui/GraphicBufferAllocator.cpp
    
    Change-Id: I8a8ef605dffc67a1bf2605c738bb43e801688371

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -39,16 +39,6 @@ LOCAL_SHARED_LIBRARIES := \
 	libutils \
 	liblog
 
-# Executed only on QCOM BSPs
-ifeq ($(TARGET_USES_QCOM_BSP),true)
-ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),)
-    LOCAL_C_INCLUDES += hardware/qcom/display-$(TARGET_QCOM_DISPLAY_VARIANT)/libgralloc
-else
-    LOCAL_C_INCLUDES += hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libgralloc
-endif
-    LOCAL_CFLAGS += -DQCOM_BSP
-endif
-
 ifeq ($(BOARD_EGL_SKIP_FIRST_DEQUEUE),true)
     LOCAL_CFLAGS += -DSURFACE_SKIP_FIRST_DEQUEUE
 endif
